301db7b7d53f9c208d0d2377454e89e2a553e5c3
Rishabh Nrupnarayan <aryannr97@gmail.com>
2022-06-01 15:48:10 +0530

    perf(batchproc): Generic batch processor implementation
    BREAKING CHANGE: batchproc pkg for generic batch processing

---
+++ test_config.go
@@ -0,0 +1,7 @@
+package batchproc
+
+// testRunKey is user defined type for unit test key to be added in request context
+type testRunKey string
+
+// testRun defines constant key for unit test
+const testRun testRunKey = "testRun"
